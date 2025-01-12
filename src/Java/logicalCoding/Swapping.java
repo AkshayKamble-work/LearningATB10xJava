fcec3f81b1cdb1363dd2f8c6d0022d51020ca69b
akshay <Akshaydkamble.01@gmail.com>
2025-01-12 19:13:49 +0530

    Swapping

---
+++ src/Java/logicalCoding/Swapping.java
@@ -0,0 +1,19 @@
+package Java.logicalCoding;
+
+public class Swapping {
+
+    public static void main(String[] args)
+    {
+    int a=10;
+    int b=20;
+        System.out.println("---Before Swapping---");
+        System.out.println("Value of A "+a);
+        System.out.println("Value of B "+b);
+        a=a+b;
+        b=a-b;
+        b=a-b;
+        System.out.println("---After Swapping---");
+        System.out.println("Value of A "+a);
+        System.out.println("Value of B "+b);
+    }
+}
