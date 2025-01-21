f937593c862482866d643b4873a38a18c58af1a1
akshay <Akshaydkamble.01@gmail.com>
2025-01-21 18:40:50 +0530

    InvertedTriangle

---
+++ src/Java/StartProgram/InvertedTriangle.java
@@ -0,0 +1,20 @@
+package Java.StartProgram;
+
+public class InvertedTriangle {
+/*
+
+ *****
+ ****
+ ***
+ **
+ *
+
+ */
+    public static void main(String[] args) {
+        int n=5;
+        for(int i=n;i>=1;i--)
+        {
+            System.out.println("*".repeat(i));
+        }
+    }
+}
