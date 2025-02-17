07b39e1636a8c58e28b35ecbb93059c3cddc2e05
akshay <Akshaydkamble.01@gmail.com>
2025-02-17 14:55:23 +0530

    Constructor

---
+++ src/Constructor/Java.java
@@ -0,0 +1,26 @@
+package Constructor;
+
+
+class Car {
+    String brand;
+    int year;
+
+    Car() {
+        this("Ford", 2020); // Calls the constructor below
+        System.out.println("Default constructor called");
+    }
+
+    Car(String b, int y) {
+        this.brand = b;
+        this.year = y;
+        System.out.println("Brand: " + brand + ", Year: " + year);
+    }
+}
+
+class Main {
+    public static void main(String[] args) {
+        Car myCar = new Car(); // Calls the default constructor
+    }
+}
+
+
