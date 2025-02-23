3ccc65de7d6d2ae0a3d97456b3a584063e877411
akshay <Akshaydkamble.01@gmail.com>
2025-02-23 14:47:09 +0530

    Vector

---
+++ src/CollectionFramework/List/List/VectorTest.java
@@ -0,0 +1,41 @@
+package CollectionFramework.List.List;
+
+import java.util.List;
+import java.util.Vector;
+
+public class VectorTest {
+
+
+    public static void main(String[] args) {
+
+        List myvector = new Vector();
+        myvector.add("Black");
+        myvector.add("White");
+        myvector.add("Blue");
+        myvector.add("Green");
+        myvector.add("Gray");
+        myvector.add("Yellow");
+        myvector.add("Darkblue");
+        myvector.add(null);
+        myvector.add("Gray");
+
+        System.out.println(myvector);
+        System.out.println("Size of linked list --> "+myvector.size());
+        System.out.println("Index of Draganfruit element is --> "+myvector.indexOf("Yellow"));
+        System.out.println("Linked list is empty --> "+myvector.isEmpty());
+        System.out.println(myvector.contains(5));
+        System.out.println(myvector.remove(null));
+        System.out.println(myvector);
+        System.out.println(myvector.getLast());
+        System.out.println(myvector.getFirst());
+        System.out.println(myvector.set(5,"Blackcherry"));
+        System.out.println(myvector);
+
+        for (int i = 0; i <myvector.size() ; i++)
+        {
+            System.out.println(myvector.get(i));
+        }
+
+
+    }
+}
