a792c843f970d5a64ed988f2c60e7ae5d82f61a5
akshay <Akshaydkamble.01@gmail.com>
2025-02-24 19:00:22 +0530

    Set

---
+++ src/CollectionFramework/List/Set/SetTest1.java
@@ -0,0 +1,26 @@
+package CollectionFramework.List.Set;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class SetTest1
+{
+
+    public static void main(String[] args) {
+        Set myset=new HashSet();
+        myset.add("Banana");
+        myset.add("Blackberry");
+        myset.add("Apple");
+        myset.add("Mango");
+        myset.add(null);// only one null are allowed
+        myset.add("Banana"); // Duplicate are not allowed
+        myset.add(null);
+        System.out.println(myset);
+        System.out.println("Size of set is --> "+myset.size());
+        System.out.println("Mango is contained ? -->  "+myset.contains("Mango"));
+        System.out.println("Set is Empty ? --> "+myset.isEmpty());
+
+
+
+    }
+}
