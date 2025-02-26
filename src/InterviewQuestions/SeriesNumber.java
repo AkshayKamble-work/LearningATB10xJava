8c8a77d2ad9f75e35a15ce640e1bea3c3ba8203d
akshay <Akshaydkamble.01@gmail.com>
2025-02-26 19:37:33 +0530

    Series and Swapping

--- src/InterviewQuestions/SeriesNumber.java
+++ src/InterviewQuestions/SeriesNumber.java
@@ -1,20 +1,30 @@
 package InterviewQuestions;
-
-public class SeriesNumber
-
-{
+import java.util.*;
+public class SeriesNumber {
 
     public static void main(String[] args) {
-        int a=-1;
-        int b=1;
+        int a = -1;
+        int b = 1;
         int c;
         System.out.print("Series number is --> ");
-        for(int i=0;i<=10;i++)
-        {
-            c=a+b;
-            a=b;
-            b=c;
-            System.out.print(c+" ,");
+        for (int i = 0; i <= 10; i++) {
+            c = a + b;
+            a = b;
+            b = c;
+            System.out.print(c + " ,");
         }
+        System.out.println();
+        System.out.println("---Swapping Number---");
+        int A=10;
+        int B=20;
+        System.out.println("---Before Swapping---");
+        System.out.println("Value of A "+A);
+        System.out.println("Value of B "+B);
+        a=a+b;
+        b=a-b;
+        a=a-b;
+        System.out.println("---After Swapping---");
+        System.out.println("Value of A "+A);
+        System.out.println("Value of B "+B);
     }
 }
