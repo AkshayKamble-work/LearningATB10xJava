5c443a0fc7dc32174c019fc8e62e17dd404d9038
akshay <Akshaydkamble.01@gmail.com>
2025-03-13 13:22:49 +0530

    sorted array 0,1,2,3,4,5,6,7,8,9,10,

--- src/Arrayss/SortArrays.java
+++ src/Arrayss/SortArrays.java
@@ -5,14 +5,24 @@ import java.util.Arrays;
 public class SortArrays {
 
     public static void main(String[] args) {
-        int a[]={10,9,8,7,5,4,6,2,1,3,0};
+        int a[] = {10, 9, 8, 7, 5, 4, 6, 2, 1, 3, 0};
         Arrays.sort(a);
         // First Way
         System.out.println(Arrays.toString(a));
         // second way
-        for(int i=0;i<a.length;i++)
-        {
-            System.out.print(a[i]+",");
+        for (int i = 0; i < a.length; i++) {
+            System.out.print(a[i] + ",");
+        }
+        System.out.println("");
+        String[] names = {"Zebra", "Apple", "Mango", "Banana", "Cherry"};
+
+        Arrays.sort(names); // Sorting the string array
+// first way
+        System.out.println("Sorted String Array: " + Arrays.toString(names));
+
+        for (int i = 0; i < names.length; i++) {
+
+            System.out.print(names[i] + ",");
         }
     }
 }
