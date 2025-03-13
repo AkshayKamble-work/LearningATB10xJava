b808e24121ea126aa46fae0ef328505f4ff5f789
akshay <Akshaydkamble.01@gmail.com>
2025-03-13 11:39:01 +0530

    final keyword

---
+++ src/FinalKeyword/ExampleFinal.java
@@ -0,0 +1,16 @@
+package FinalKeyword;
+
+public class ExampleFinal {
+
+    final int x = 10; // Final variable
+
+    void changeValue() {
+        x = 20;  //❌ Error: Cannot assign a value to cannot assign a value to final variable xfinal variable 'x'
+
+    }
+
+    public static void main(String[] args) {
+        ExampleFinal ex=new ExampleFinal();
+        ex.changeValue();// throw the error beacause we can not change the value of final keyword
+    }
+}
