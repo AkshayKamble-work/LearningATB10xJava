d920bbd18dd72b5eaaa1c6ab2ebf3ccd91c2e6e5
akshay <Akshaydkamble.01@gmail.com>
2025-03-24 21:48:47 +0530

    // String s="Automation555Testing";     // output 25 25 25

--- src/InterviewQuestions/SquareDigitsFromString.java
+++ src/InterviewQuestions/SquareDigitsFromString.java
@@ -6,7 +6,7 @@ public class SquareDigitsFromString {
 
     public static void main(String[] args)
     {
-        String s="Automation555Testing";
+        String s="Automation987Testing";
         int digit;
         for(char ch:s.toCharArray())
         {
@@ -17,5 +17,23 @@ public class SquareDigitsFromString {
                 System.out.println("Square of " + digit + " is: " + square);
             }
         }
+
+// Addition of integers in string
+       // input  "Automation884Testing"
+        // output 20
+        String name = "Automation884Testing";
+int totalSum=0;
+        // Loop through each character in the string
+        for (char ch : name.toCharArray()) {
+            // Check if the character is a digit
+            if (Character.isDigit(ch)) {
+
+                int digits = Character.getNumericValue(ch); // Convert char to int
+                totalSum += digits; // Add to total sum
+            }
+
+        }
+        System.out.println("Total sum of digits is: " + totalSum);
+
     }
 }
